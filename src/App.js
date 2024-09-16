18484e92dda5684d21dac032c5f9d54e51a828c7
chaanakyaaM <chaanakyaam@gmail.com>
2024-09-16 20:10:16 +0530

    add to watchlist feature completed

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             <div className="two"><MovieDetails Id={id} plotsetter={setPlot} watchlistsetter={setwatchlist} watchlist={watchlist}></MovieDetails></div>
             <div className="center">MoviesHub</div>
             <div className="three"><span>Plot</span> : <i>{plot ? plot : 'Plot goes here...'}</i></div>
-            <div className="four"><WatchList datalist={watchlist}/></div>
+            <div className="four"><WatchList datalist={watchlist} watchlistsetter={setwatchlist}/></div>
             </Container>}>
           </Route>
           <Route path="/about" element={'/'}>
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import DeleteImg from '../delete-icon.png';
 
-function WatchList({datalist}) {
+function WatchList({datalist,watchlistsetter}) {
   const uniqueDatalist = Array.from(new Set(datalist.map(item => JSON.stringify(item)))).map(item => JSON.parse(item));
+  function delhandler(title, year) {
+    const updatedList = uniqueDatalist.filter(item => item.title !== title || item.year !== year);
+    watchlistsetter(updatedList);
+  }
   return (
     <div className='watchlist-container'>
         <h2>Watch List</h2>
@@ -12,7 +16,7 @@ function WatchList({datalist}) {
             <div className="watchlist-details">
             <p>Title : {item.title}</p>
             <p>Year : {item.year}</p>
-            <button className='del'><img src={DeleteImg} alt="" /></button>
+            <button className='del' onClick={()=>delhandler(item.title,item.year)}><img src={DeleteImg} alt="" /></button>
             </div>
         </div>
     ))}
